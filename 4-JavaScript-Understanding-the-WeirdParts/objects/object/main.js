e089f51686d82bb08e822713c950be2494459896
cihat <cihat02020202@gmail.com>
2021-05-06 15:23:39 +0300

    update object file

--- 4-JavaScript-Understanding-the-WeirdParts/objects/object/main.js
+++ 4-JavaScript-Understanding-the-WeirdParts/objects/object/main.js
@@ -28,7 +28,7 @@ spanish.greet = "Hola!"
 
 console.log(english) */
 
-//! JSON 
+//! JSON
 /* var objectLiteral = {
   firstname: 'John',
   isAProgrammer: true
@@ -46,7 +46,6 @@ console.log(jsonValue) */
 
 greet.language = 'Turkish'
 console.log(greet.language) */
-
 
 //! Function Expression --- TAnımlandığından daha önce çağırılırsa hata verir!!! Bir primitive value gibi.
 /* var anonymousGreet = function() {
@@ -66,15 +65,15 @@ log(() => {
 }) */
 
 //! primitive value'larda referance farklıdır ama bu objectler'de boyle değildir reference aynıdır yaptığın değişiklik ilgili her noktayı etkiler !!!DİKKAT ETTT.
-let a = 123;
+/* let a = 123;
 let b;
 b = a;
 b = 1;
 console.log("b değeri: ", b)
-console.log("a değeri: ", a)
+console.log("a değeri: ", a) */
 
 //! by reference (all objects (including functions))
-var c = {
+/* var c = {
   greeting: 'Hi..',
 }
 var d;
@@ -83,20 +82,20 @@ d = c;
 d.greeting = 'Merhaba' //! mutate
 
 console.log("c: ", c)
-console.log("d: ", d)
+console.log("d: ", d) */
 
 //! by reference (even as parameters)
-function changeGreeting(obj) {
+/* function changeGreeting(obj) {
   obj.greeting = 'Hola'; //! Mutate
 }
 
 changeGreeting(d)
 console.log("c: ", c);
-console.log("d: ", d)
+console.log("d: ", d) */
 
 //! equals operator sets up new memory space (new address)
 //! burada önemli bir nokta var js'de objectler reference ile çalışır, primitive value'larda ise boyle bişi yoktur.
-c = {
+/* c = {
   greeting: 'howdy'
 }
 
@@ -104,4 +103,111 @@ d = {
   
 }
 console.log("c: ", c);
-console.log("d: ", d)
+console.log("d: ", d) */
+
+/* function a() {
+  console.log(this);
+  this.newVariable = "Hello";
+}
+
+var b = function () {
+  console.log(this);
+};
+
+a();
+console.log(newVariable);
+b(); */
+/* 
+var c = {
+  name: "The C object",
+  log: function () {
+    this.name = 'Updated C object';
+    // console.log(this);
+    var self = this;
+
+    var setname = function (newname) {
+      self.name = newname;
+    }
+    setname('Updated again! The c object')
+    console.log(self)
+  },
+  show() {
+    console.log(this);
+  },
+};
+
+c.log();
+c.show()
+ */
+
+/* var arr = [
+  1,
+  false,
+  { name: "Tony", address: "111 Main St." },
+  function (name) {
+    var greeting = "Hello ";
+    console.log(greeting + name);
+  },
+  "Hello",
+];
+
+console.log(arr)
+arr[3](arr[2].name  ) */
+
+/* function greet(firstname, lastname, language, ...other) {
+  language = language || 'en'
+
+  if (arguments.length === 0) {
+    console.log('Missing Parameters!')
+    console.log("---------------")
+    return;
+  }
+
+  console.log(firstname)
+  console.log(lastname)
+  console.log(language)
+  console.log('arg 0: ' + arguments[0])
+  console.log('Other', other)
+  console.log("------------------")
+}
+
+greet();
+greet('John')
+greet('John', 'Doe')
+greet('John', 'Doe', 'tr')
+greet('John', 'Doe', 'tr', "111 Barbaros", 'sadşkljsd', "sdşlkfjav", "şlkadjds") */
+
+//! IIEF(Immediately Invoked Function Expression)
+/* var greeting = (name => {
+  return 'Hello ' + name;
+})("John Doe")
+
+console.log(greeting)
+
+3;
+"Helllo I am a string";
+
+{
+  name: 'John'
+}
+var firstname = 'John'
+(function (name) {
+  console.log('Inside IIFE: Hello ' + name)
+})(firstname)//! IIFE */
+
+//! IIFE
+/* (function (global, name) {
+  var greeting = 'Hello'
+  global.greeting = 'Hello'
+  console.log(greeting + ' ' + name)
+})(window, 'John')//! IIFE
+
+console.log(greeting) */
+
+function greet(whattosay) {
+  return function (name) {
+    console.log(whattosay + ' ' + name)
+  }
+}
+
+greet('Hi')("Tony")
